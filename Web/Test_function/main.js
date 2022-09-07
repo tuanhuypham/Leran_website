e886c73202c4ccd908d7c58358a50b2428c6fa9e
huypham <huy5153a@gmail.com>
2022-09-07 16:16:03 +0700

    update

---
+++ Web/Test_function/main.js
@@ -0,0 +1,5 @@
+const clickOn = document.querySelector('.menu');
+
+function on() {
+    clickOn.classList.toggle('menu_on')
+}
--- Web/chat email/main.js
+++ Web/chat email/main.js
@@ -1,4 +1,12 @@
-const listClick = document.querySelector('.list');
-function showList() {
-    listClick.classList.add('list_on');
+const disPlayon = document.querySelector('.control-extended');
+function onList(){
+    disPlayon.classList.toggle('control-extended_on');
+}
+const onSubmenu =document.querySelector('.sub-item-option');
+function disPlayonSubmenu () {
+    onSubmenu.classList.toggle('sub-item-option-mail-on');
+}
+const onThreeDots = document.querySelector('.three-dots');
+function threeDotsOn () {
+    onThreeDots.classList.toggle('three-dots-on');
 }
