c15d47aa0f5d3cca2a9e18f5b753a71dcd4c1809
huypham <huy5153a@gmail.com>
2022-09-14 19:11:02 +0700

    update

--- Web/health/main.js
+++ Web/health/main.js
@@ -1,5 +1,5 @@
 let slideIndex = 1;
-showSlides(slideIndex);
+
 
 function plusSlides(n) {
     showSlides(slideIndex += n);
@@ -13,14 +13,28 @@ function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }    
+    
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    
     for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+        slides[i].style.display = "none";  
     }
+    
     for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace(" active", "");
     }
+    
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
 }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    showSlides(slideIndex);
+});
